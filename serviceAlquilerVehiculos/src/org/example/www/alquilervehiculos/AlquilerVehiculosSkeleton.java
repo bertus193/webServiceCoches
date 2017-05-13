763a5aac800f90ccfcb5d3877d8db5dcb3b964cd
sergi <frances1991@hotmail.com>
2017-05-13 14:19:49 +0200

    Alquiler vehiculos services inicial

---
+++ serviceAlquilerVehiculos/src/org/example/www/alquilervehiculos/AlquilerVehiculosSkeleton.java
@@ -0,0 +1,32 @@
+
+/**
+ * AlquilerVehiculosSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.3  Built on : Jun 27, 2015 (11:17:49 BST)
+ */
+    package org.example.www.alquilervehiculos;
+    /**
+     *  AlquilerVehiculosSkeleton java skeleton for the axisService
+     */
+    public class AlquilerVehiculosSkeleton{
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param obtenerVehiculos 
+             * @return obtenerVehiculosResponse 
+         */
+        
+                 public org.example.www.alquilervehiculos.ObtenerVehiculosResponse obtenerVehiculos
+                  (
+                  org.example.www.alquilervehiculos.ObtenerVehiculos obtenerVehiculos
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#obtenerVehiculos");
+        }
+     
+    }
+    
